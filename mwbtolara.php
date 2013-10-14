72309e9e5c1d0077193eea9b010051356600b12c
arcadas <perger1984@gmail.com>
2013-10-14 20:43:10 +0200

    dev

--- mwbtolara.php
+++ mwbtolara.php
@@ -1,10 +1,43 @@
 <?php
 
+// .mwb parser
 require_once 'MWB.php';
-$sPath = '..';
-$oMWB = new MWB($sPath.'/tmp/laravel.mwb');
+$oMWB = new MWB('../tmp/laravel.mwb');
 $aMWBTables = $oMWB->getMWBTablesArray();
 
+// .mwb xml parser
+
+// database name and charset
+
+// database tables (name and charset)
+
+// table fields (name, type, properties, default, comment)
+
+// foreign keys
+
+// tables connections
+
+
+// laravel migrations and models generator
 require_once 'Lara.php';
 $oLara = new Lara;
 $oLara->generateMigrationsAndModels($aMWBTables);
+
+// generate migrations
+
+// generate foreign file
+
+// generate model with placeholders
+
+// set model base class
+
+// set Optional namespace
+
+// set Mass Assignment (guarded id)
+
+// set Ardent
+
+// set Factory Muff
+
+// set validator texts by comment
+
