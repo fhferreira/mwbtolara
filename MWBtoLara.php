0b46f71b1914918b978039b289972315a2a364b3
arcadas <perger1984@gmail.com>
2013-10-14 15:33:15 +0200

    dev

--- MWBtoLara.php
+++ MWBtoLara.php
@@ -1,11 +1,10 @@
 <?php
 
 require_once 'MWB.php';
-require_once 'Lara.php';
-
 $sPath = '..';
 $oMWB = new MWB($sPath.'/tmp/laravel.mwb');
 $aMWBTables = $oMWB->getMWBTablesArray();
 
+require_once 'Lara.php';
 $oLara = new Lara;
 $oLara->generateMigrationsAndModels($aMWBTables);
